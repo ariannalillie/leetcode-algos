8f91ea31fda4099ba48eddebeff8071cf9342ecf
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-18 16:02:10 -0400

    complete search insert position

--- search-insert-position.js
+++ search-insert-position.js
@@ -3,5 +3,17 @@
 // the index where it would be if it were inserted in order.
 
 const searchInsertPosition = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
+    }
+    if (nums[i] > target) {
+     return i;
+    }
+  }
+  return nums.length;
+};
 
-}
+const nums = [1,3,5,6]
+const target = 2;
+console.log(searchInsertPosition(nums, target)); // Output: 2
