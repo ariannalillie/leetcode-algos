cda4fb8fb8916a275dd9ea80feac1bd3d939d59d
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-19 07:23:15 -0400

    sorted squares complete

--- squares-sorted-array.js
+++ squares-sorted-array.js
@@ -1,10 +1,13 @@
 // Given an integer array nums sorted in non-decreasing order
-// return an array of the squares of each number 
+// return an array of the squares of each number
 // sorted in non-decreasing order
 
 const sortedSquares = (nums) => {
-
+  const squared = nums.map((num) => {
+    return num * num;
+  });
+  return squared.sort((a, b) => a - b);
 };
 
-const nums = [-7,-3,2,3,11]
-console.log(sortedSquares(nums)) // Output: [4,9,9,49,121]
+const nums = [-7, -3, 2, 3, 11];
+console.log(sortedSquares(nums)); // Output: [4,9,9,49,121]
