2638e53982211b7d9b20c4b8e3cebec4adbb83b7
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-19 14:22:14 -0400

    kids with candies complete;

--- kids-with-candies.js
+++ kids-with-candies.js
@@ -1,14 +1,21 @@
-// Return a boolean array result of length n, where result[i] is true if, 
-// after giving the ith kid all the extraCandies, they will have the greatest 
+// Return a boolean array result of length n, where result[i] is true if,
+// after giving the ith kid all the extraCandies, they will have the greatest
 // number of candies among all the kids, or false otherwise.
 
 const kidsWithCandies = (candies, extraCandies) => {
+  let booleanArr = [];
+  const kidWithMostCandies = Math.max(...candies);
+  candies.ForEach((kid) => {
+    kid + extraCandies >= kidWithMostCandies
+      ? booleanArr.push(true)
+      : booleanArr.push(false);
+  });
+  return booleanArr;
+};
 
-}
-
-const candies = [2,3,5,1,3]
-const extraCandies = 3
-console.log(kidsWithCandies(candies, extraCandies)) // Output: [true,true,true,false,true] 
+const candies = [2, 3, 5, 1, 3];
+const extraCandies = 3;
+console.log(kidsWithCandies(candies, extraCandies)); // Output: [true,true,true,false,true]
 // Explanation: If you give all extraCandies to:
 // - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
 // - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
