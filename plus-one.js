4390022a7b91e8b7112e12ed89609fe69dd4602f
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-19 10:27:59 -0400

    plus one complete

--- plus-one.js
+++ plus-one.js
@@ -1,20 +1,20 @@
-// Increment the large integer by one and return the resulting 
+// Increment the large integer by one and return the resulting
 // array of digits.
 
 const plusOne = (digits) => {
-    if (digits.length === 1) digits.unshift(0);
-    for(let i = digits.length -1; i >= 0; i--) {
-        if (digits[i] === 9) {
-            digits[i] = 0;
-            continue;
-        }
-        digits[i]+=1;
-        break;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      return digits;
+    } else {
+      digits[i] = 0;
     }
-    return digits;
-}
+  }
+  digits.unshift(1);
+  return digits;
+};
 
-const digits1 = [1,2,9];
-const digits2 = [9]
-console.log(plusOne(digits1)) // Output: [1,2,4]
-console.log(plusOne(digits2)) // Output: [1,0]
+const digits1 = [1, 2, 9];
+const digits2 = [9];
+console.log(plusOne(digits1)); // Output: [1,2,4]
+console.log(plusOne(digits2)); // Output: [1,0]
