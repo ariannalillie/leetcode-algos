8de02cd2b1712eb9ce2fcff40913385d60f57f46
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-19 07:27:09 -0400

    rotate setup

---
+++ rotate-array.js
@@ -0,0 +1,10 @@
+// Given an array rotate the array to the right by K steps,
+// where K is non-negative.
+
+const rotate = (nums, k) => {
+
+};
+
+const nums = [1,2,3,4,5,6,7]
+const k = 3
+console.log(rotate(nums, k)) // Output: [5,6,7,1,2,3,4]
