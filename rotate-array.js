e98602721d1d2e48df6745a409aad27c96f78745
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-19 08:08:32 -0400

    rotate complete

--- rotate-array.js
+++ rotate-array.js
@@ -1,10 +1,16 @@
 // Given an array rotate the array to the right by K steps,
 // where K is non-negative.
+// SORT IN PLACE
 
 const rotate = (nums, k) => {
-
+  let i = 0;
+  while (i < k) {
+    i++;
+    nums.unshift(nums.pop());
+  }
+  return nums;
 };
 
-const nums = [1,2,3,4,5,6,7]
-const k = 3
-console.log(rotate(nums, k)) // Output: [5,6,7,1,2,3,4]
+const nums = [-1, -100, 3, 99];
+const k = 2;
+console.log(rotate(nums, k)); // Output: [5,6,7,1,2,3,4]
