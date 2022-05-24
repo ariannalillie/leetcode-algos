d7393aca0c8196fa9124f297deda26f18270596e
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-24 11:11:28 -0400

    reverse word complete

--- reverse-word-string.js
+++ reverse-word-string.js
@@ -1,6 +1,11 @@
-const reverseWords = (string) => {
-
-}
+const reverseWords = (s) => {
+  const newArr = [];
+  const wordsArr = s.split(" ");
+  wordsArr.forEach((word) => {
+    newArr.push([...word].reverse().join(""));
+  });
+  return newArr.join(" ");
+};
 
 const s = "Let's take LeetCode contest";
-console.log(reverseWords(s)) // Output: "s'teL ekat edoCteeL tsetnoc"
+console.log(reverseWords(s)); // Output: "s'teL ekat edoCteeL tsetnoc"
