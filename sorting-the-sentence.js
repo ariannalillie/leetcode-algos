fc0c1e301d1539a891997f8936b2dfad339f0e3d
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-25 13:59:31 -0400

    sort sentence set up

---
+++ sorting-the-sentence.js
@@ -0,0 +1,8 @@
+const sortSentence = () => {
+
+};
+
+const s = "is2 sentence4 This1 a3"
+console.log(sortSentence(s)) // Output: "This is a sentence"
+// Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", 
+// then remove the numbers.
