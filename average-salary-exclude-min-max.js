8cccd7b72c65be4a0980a46d70292e977fd15186
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-26 13:30:15 -0400

    average salary set up

---
+++ average-salary-exclude-min-max.js
@@ -0,0 +1,8 @@
+const average = (salary) => {
+
+}
+
+const salary = [4000,3000,1000,2000];
+console.log(average(salary)) // Output: 2500.00000
+// Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+// Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
