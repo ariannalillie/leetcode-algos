d18af8273e20583d6b23aa3a2913fe32b093bb85
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-26 08:33:37 -0400

    target indicies complete

--- target-indices-after-sort.js
+++ target-indices-after-sort.js
@@ -1,9 +1,16 @@
 const targetIndices = (nums, target) => {
+  let indicesArr = [];
+  const sorted = nums.sort((a, b) => a - b);
+  sorted.forEach((num, idx) => {
+    if (num === target) {
+      indicesArr.push(idx);
+    }
+  });
+  return indicesArr;
+};
 
-}
-
-const nums = [1,2,5,2,3]
-const target = 2
-console.log(targetIndices(nums, target)) // Output: [1,2]
+const nums = [1, 2, 5, 2, 3];
+const target = 2;
+console.log(targetIndices(nums, target)); // Output: [1,2]
 // Explanation: After sorting, nums is [1,2,2,3,5].
 // The indices where nums[i] == 2 are 1 and 2.
