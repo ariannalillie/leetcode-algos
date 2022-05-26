a8a5c72f96349c6f07bf315ca59dee7b37471322
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-26 08:30:29 -0400

    targetindicies after sort setup;

---
+++ target-indices-after-sort.js
@@ -0,0 +1,9 @@
+const targetIndices = (nums, target) => {
+
+}
+
+const nums = [1,2,5,2,3]
+const target = 2
+console.log(targetIndices(nums, target)) // Output: [1,2]
+// Explanation: After sorting, nums is [1,2,2,3,5].
+// The indices where nums[i] == 2 are 1 and 2.
