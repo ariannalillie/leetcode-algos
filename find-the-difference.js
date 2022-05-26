c10c3560a6c452bde6525a0e945ae53d1ba97a4b
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-26 10:56:06 -0400

    working on solution

--- find-the-difference.js
+++ find-the-difference.js
@@ -1,8 +1,18 @@
 const findTheDifference = (s, t) => {
-  
+  let differentChar;
+  const tAsArr = t.split("");
+  const sAsArr = s.split("");
+  tAsArr.forEach((char, idx) => {
+    if (!sAsArr.includes(char)) {
+      differentChar = char;
+    } else {
+        sAsArr.splice(idx, 1)
+    }
+  });
+  return differentChar;
 };
 
-const s = "abcd"
-const t = "abcde"
-console.log(findTheDifference(s, t)) // Output: "e"
+const s = "a";
+const t = "aa";
+console.log(findTheDifference(s, t)); // Output: "e"
 // Explanation: 'e' is the letter that was added.
