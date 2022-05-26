65a5258b34f8068b7be23b883ae876abce7ae0a9
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-26 10:46:07 -0400

    find the difference setup

---
+++ find-the-difference.js
@@ -0,0 +1,8 @@
+const findTheDifference = (s, t) => {
+  
+};
+
+const s = "abcd"
+const t = "abcde"
+console.log(findTheDifference(s, t)) // Output: "e"
+// Explanation: 'e' is the letter that was added.
