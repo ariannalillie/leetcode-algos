bc1b8fb58caf402b38ea5b8940e959a42e920814
Arianna Johnson <ariannajohnson588@gmail.com>
2022-05-26 10:21:40 -0400

    max product difference setup

---
+++ max-product-different-between-pairs.js
@@ -0,0 +1,8 @@
+const maxProducrDifference = (nums) => {
+
+};
+
+const nums = [5,6,2,7,4]
+console.log(maxProducrDifference(nums)) // Output: 34
+// Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+// The product difference is (6 * 7) - (2 * 4) = 34.
