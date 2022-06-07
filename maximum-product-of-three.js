3ee0eab56dc36fca0be3e7d2bedd8381147ac72d
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-07 13:18:08 -0400

    maimum product complete

--- maximum-product-of-three.js
+++ maximum-product-of-three.js
@@ -1,6 +1,10 @@
 const maximumProduct = (nums) => {
-
+  nums.sort((a, b) => a - b);
+  return Math.max(
+    nums[0] * nums[1] * nums[nums.length - 1],
+    nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]
+  );
 };
 
-const nums = [1,2,3];
-console.log(maximumProduct(nums)) // Output 6
+const nums = [-100, -98, -1, 2, 3, 4];
+console.log(maximumProduct(nums)); // Output 39200
