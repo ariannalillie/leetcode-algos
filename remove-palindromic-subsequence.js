d5cce1024196f1d3e2c435c87a003ba1eca0faf5
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-08 08:22:24 -0400

    remove palindrome sub setup

---
+++ remove-palindromic-subsequence.js
@@ -0,0 +1,8 @@
+const removePalindromeSub = (s) => {
+
+}
+
+const s = "ababa"
+console.log(removePalindromeSub(s)) // Output: 1
+// Explanation: s is already a palindrome, so its entirety can be 
+// removed in a single step.
