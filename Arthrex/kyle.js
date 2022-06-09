67ee240c1fb0c93ce818c0f5b04386ad57a21c4b
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-09 14:03:41 -0400

    helping kyle

--- Arthrex/kyle.js
+++ Arthrex/kyle.js
@@ -1,13 +1,24 @@
-const pipedMetadata = (duration, language, date, referenceNumber) => {
-  const data = [duration, language, date, referenceNumber];
-  let piped = "";
+// const pipedMetadata = (duration, language, date, referenceNumber) => {
+//   const data = [duration, language, date, referenceNumber];
+//   let piped = "";
 
-  data.forEach((el) => {
-    el ? (piped += `${el} | `) : "";
-  });
-  
-  return piped.slice(0, piped.length - 3);
-};
+//   data.forEach((el) => {
+//     el ? (piped += `${el} | `) : "";
+//   });
+
+//   return piped.slice(0, piped.length - 3);
+// };
+
+const pipedMetadata = (duration, language, date, referenceNumber) => {
+    const data = [duration, language, date, referenceNumber];
+    
+    let piped = data.map(el => {
+        return el ? `${el} | ` : "";
+    })
+
+    let pipedString = piped.join('');
+    return pipedString.slice(0, pipedString.length - 3)
+  };
 
 const duration = "22:46:40";
 const language = null;
