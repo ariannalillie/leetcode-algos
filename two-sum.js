302cb8a98d277efff87337ff81c6f7625a578b34
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-09 08:40:17 -0400

    two sum setup

---
+++ two-sum.js
@@ -0,0 +1,9 @@
+const twoSum = (numbers, target) => {
+
+}
+
+const numbers = [2,7,11,15]
+const target = 9;
+console.log(twoSum(numbers, target)) // Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, 
+// index1 = 1, index2 = 2. We return [1, 2].
