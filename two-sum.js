165358fb472aa98e20b62d9c431bd608529f2dfe
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-09 08:50:08 -0400

    currently solution

--- two-sum.js
+++ two-sum.js
@@ -1,9 +1,15 @@
 const twoSum = (numbers, target) => {
+  let indexArr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers.includes(target - numbers[i])) {
+      indexArr.push(i + 1);
+    }
+  }
+  return indexArr;
+};
 
-}
-
-const numbers = [2,7,11,15]
+const numbers = [2, 7, 11, 15];
 const target = 9;
-console.log(twoSum(numbers, target)) // Output: [1,2]
-// Explanation: The sum of 2 and 7 is 9. Therefore, 
+console.log(twoSum(numbers, target)); // Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore,
 // index1 = 1, index2 = 2. We return [1, 2].
