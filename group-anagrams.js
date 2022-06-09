5bd8b29c2f46e7944fce3c3ca6f602eba9c4285d
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-09 14:34:26 -0400

    group anagrams complete

--- group-anagrams.js
+++ group-anagrams.js
@@ -1,10 +1,11 @@
 const groupAnagrams = (strs) => {
-    const sortedStrArr = []
-    strs.forEach(str => {
-        sortedStrArr.push(str.split("").sort().join(""))
-    })
-    return sortedStrArr;
-    
-}
-const strs = ["eat","tea","tan","ate","nat","bat"];
-console.log(groupAnagrams(strs)) // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+  sortedStrings = strs.map((word) => word.split("").sort().join(""));
+  let hash = {};
+  for (let i in strs) {
+    if (!hash[sortedStrings[i]]) hash[sortedStrings[i]] = [strs[i]];
+    else hash[sortedStrings[i]].push(strs[i]);
+  }
+  return Object.values(hash);
+};
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+console.log(groupAnagrams(strs)); // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
