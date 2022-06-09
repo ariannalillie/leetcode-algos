4b22cbda2051416162bf4acf6a9162826428471f
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-09 13:42:24 -0400

    helping kyle

---
+++ Arthrex/kyle.js
@@ -0,0 +1,16 @@
+const pipedMetadata = (duration, language, date, referenceNumber) => {
+  const data = [duration, language, date, referenceNumber];
+  let piped = "";
+
+  data.forEach((el) => {
+    el ? (piped += `${el} | `) : "";
+  });
+  return piped.slice(0, piped.length - 3);
+};
+
+const duration = "22:46:40";
+const language = null;
+const date = "03/18/2013";
+const referenceNumber = "UE264UE264";
+
+console.log(pipedMetadata(duration, language, date, referenceNumber));
