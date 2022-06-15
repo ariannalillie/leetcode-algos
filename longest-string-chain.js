b5c541a18d040c4eedf2f2690feb51ddf8afa2e9
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-15 09:44:36 -0400

    is valid set up

---
+++ longest-string-chain.js
@@ -0,0 +1,8 @@
+const longestStrChain = (words) => {
+
+}
+
+const words = ["a","b","ba","bca","bda","bdca"]
+console.log(longestStrChain(words)) // Output: 4
+// Explanation: One of the longest word chains is 
+// ["a","ba","bda","bdca"].
---
+++ valid-parentheses.js
@@ -0,0 +1,6 @@
+const isValid = (s) => {
+
+}
+
+const s = "()[]{}"
+console(isValid(s)) // Output: true
