3d173b69b28d823288d77ff26cbd56c43f6c7f3e
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-15 09:54:27 -0400

    attempt one valid parentheses

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,6 +1,12 @@
 const isValid = (s) => {
+  sAsArr = s.split("");
+  for (let i = 0; i < sAsArr.length; i += 2) {
+    if (sAsArr[i] === "(" && !(sAsArr[i + 1] === ")")) return false;
+    if (sAsArr[i] === "{" && !(sAsArr[i + 1] === "}")) return false;
+    if (sAsArr[i] === "[" && !(sAsArr[i + 1] === "]")) return false;
+  }
+  return true;
+};
 
-}
-
-const s = "()[]{}"
-console(isValid(s)) // Output: true
+const s = "(]"
+console.log(isValid(s)); // Output: true
