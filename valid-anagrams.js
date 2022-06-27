f304a64c3cd2b746ec2a7aa88f49a6903b2b09f0
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-27 14:18:53 -0400

    valid anagram solution 1

--- valid-anagrams.js
+++ valid-anagrams.js
@@ -1,7 +1,8 @@
 const isAnagram = (s, t) => {
-    
+    return s.split('').sort().join('') === t.split('').sort().join('')
 }
 
 const s = "anagram"
 const t = "nagaram"
-console.log(isAnagram) // Output: true
+console.log(isAnagram(s, t)) // Output: true
+
