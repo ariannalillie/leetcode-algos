82d99aa8af83840a7f2cd24a0662d3222004644b
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-27 10:00:03 -0400

    singleNumber setup

---
+++ single-number.js
@@ -0,0 +1,13 @@
+// Given a non-empty array of integers nums, 
+// every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity 
+// and use only constant extra space.
+
+const singleNumber = (nums) => {
+
+};
+
+const nums = [2, 2, 1];
+console.log(singleNumber(nums)); // Output: 1
+
