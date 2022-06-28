6a3c43a3147fd093d34149558109c7c47fd7f8ef
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 11:06:28 -0400

    min delete setup

---
+++ min-delete.js
@@ -0,0 +1,10 @@
+const minDelete = (s) => {
+
+};
+
+const s = "aaabbbcc"
+console.log(minDelete(s)) // Output: 2
+// Explanation: You can delete two 'b's r
+// esulting in the good string "aaabcc".
+// Another way it to delete one 'b' and one 'c' 
+// resulting in the good string "aaabbc".
