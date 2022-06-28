8cdb2fbe35e0c32d1a070cc8d1d6c87a3526b155
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 10:06:13 -0400

    shuffle string complete

--- shuffle-string.js
+++ shuffle-string.js
@@ -1,9 +1,15 @@
 const shuffleString = (s, indices) => {
+  const stringArray = s.split("");
+  let orderedArray = [];
 
-}
+  for (let i = 0; i < indices.length; i++) {
+    orderedArray[indices[i]] = stringArray[i];
+  }
+  return orderedArray.join('');
+};
 
 const s = "codeleet";
-const indices = [4,5,6,7,0,2,1,3];
-console.log(shuffleString(s, indices)) // Output: "leetcode"
-// Explanation: 
+const indices = [4, 5, 6, 7, 0, 2, 1, 3];
+console.log(shuffleString(s, indices)); // Output: "leetcode"
+// Explanation:
 // As shown, "codeleet" becomes "leetcode" after shuffling.
