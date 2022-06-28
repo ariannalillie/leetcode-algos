eb6b4d7460941ef97dd36e33e89311c0e2d603bc
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 10:23:14 -0400

    truncate sentence complete

--- truncate-sentence.js
+++ truncate-sentence.js
@@ -1,10 +1,12 @@
 const truncateSentence = (s, k) => {
+  let splitSentence = s.split(" ");
+  const truncSentence = splitSentence.splice(0, k);
+  return truncSentence.join(" ");
+};
 
-}
-
-const  s = "Hello how are you Contestant"
-const k = 4
-console.log(truncateSentence(s,k)) // Output: "Hello how are you"
+const s = "Hello how are you Contestant";
+const k = 4;
+console.log(truncateSentence(s, k)); // Output: "Hello how are you"
 // Explanation:
 // The words in s are ["Hello", "how" "are", "you", "Contestant"].
 // The first 4 words are ["Hello", "how", "are", "you"].
