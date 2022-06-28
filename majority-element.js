afac3208b0a928e4cd2b4ad36e6c4250b9787c27
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 09:03:37 -0400

    majority element complete

--- majority-element.js
+++ majority-element.js
@@ -1,6 +1,22 @@
 const majorityElement = (nums) => {
+  let numCountObj = {};
 
-}
+  nums.forEach((num) => {
+    !numCountObj[num] ? (numCountObj[num] = 1) : (numCountObj[num] += 1);
+  });
 
-const nums = [2,2,1,1,1,2,2];
-console.log(majorityElement(nums)) // Output: 2
+  for (let num in numCountObj) {
+    if (numCountObj[num] > nums.length / 2) return num;
+  }
+};
+
+const nums = [2, 2, 1, 1, 1, 2, 2];
+console.log(majorityElement(nums)); // Output: 2
+
+// for (let i = 0; i < nums.length; i++) {
+//     if (!map[nums[i]]) {
+//         map[nums[i]] = 1;
+//     } else if (map[nums[i]]) {
+//         map[nums[i]] += 1;
+//     }
+// }
