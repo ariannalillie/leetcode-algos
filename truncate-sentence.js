9c6e0b65cec0f7164890836dc1c28530a24af36b
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 10:20:11 -0400

    truncate sentence setup

---
+++ truncate-sentence.js
@@ -0,0 +1,11 @@
+const truncateSentence = (s, k) => {
+
+}
+
+const  s = "Hello how are you Contestant"
+const k = 4
+console.log(truncateSentence(s,k)) // Output: "Hello how are you"
+// Explanation:
+// The words in s are ["Hello", "how" "are", "you", "Contestant"].
+// The first 4 words are ["Hello", "how", "are", "you"].
+// Hence, you should return "Hello how are you".
