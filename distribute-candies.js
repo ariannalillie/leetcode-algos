ce8408e3f651a473d32b7eeb2a496b41759f7bec
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 07:43:53 -0400

    distribute candies setup

---
+++ distribute-candies.js
@@ -0,0 +1,8 @@
+const distributeCandies = (candyType) => {
+
+}
+
+const candyType = [1,1,2,2,3,3]
+console.log(distributeCandies(candyType)) // Output: 3
+// Explanation: Alice can only eat 6 / 2 = 3 candies. 
+// Since there are only 3 types, she can eat one of each type.
