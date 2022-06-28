8c99db32b9327a7f8b10e3b2441888a642e4b848
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 07:39:44 -0400

    array strings are equal

--- array-strings-are-equal.js
+++ array-strings-are-equal.js
@@ -1,10 +1,10 @@
 const arrayStringsAreEqual = (word1, word2) => {
-
+  return word1.join("") == word2.join("");
 };
 
-const word1 = ["ab", "c"]
-const word2 = ["a", "bc"]
-console.log(arrayStringsAreEqual(word1, word2)) // Output: true
+const word1 = ["ab", "c"];
+const word2 = ["a", "bc"];
+console.log(arrayStringsAreEqual(word1, word2)); // Output: true
 // Explanation:
 // word1 represents string "ab" + "c" -> "abc"
 // word2 represents string "a" + "bc" -> "abc"
