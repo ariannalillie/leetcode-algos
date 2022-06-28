d49fca79fae96d8f45ce644600f359c754b9c407
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 06:54:22 -0400

    isPalindrome setup

---
+++ valid-palindrome.js
@@ -0,0 +1,12 @@
+// A phrase is a palindrome if, after converting all uppercase 
+// letters into lowercase letters and removing all non-alphanumeric characters, 
+// it reads the same forward and backward. Alphanumeric characters include letters
+// and numbers.
+
+const isPlaindrome = (s) => {
+
+};
+
+const s = "A man, a plan, a canal: Panama";
+console.log(isPlaindrome(s)) // Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
