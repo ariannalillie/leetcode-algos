a41a0218d5d8b17ef86fa975c4d05cdd4acc0d59
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 09:17:36 -0400

    shuffle string setup

---
+++ shuffle-string.js
@@ -0,0 +1,9 @@
+const shuffleString = (s, indices) => {
+
+}
+
+const s = "codeleet";
+const indices = [4,5,6,7,0,2,1,3];
+console.log(shuffleString(s, indices)) // Output: "leetcode"
+// Explanation: 
+// As shown, "codeleet" becomes "leetcode" after shuffling.
