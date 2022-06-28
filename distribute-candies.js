8276b459e310586c4e3068ef8b6c4da629c2c8ea
Arianna Johnson <ariannajohnson588@gmail.com>
2022-06-28 07:50:08 -0400

    distribute candies complete

--- distribute-candies.js
+++ distribute-candies.js
@@ -1,8 +1,10 @@
 const distributeCandies = (candyType) => {
+  const totalCandies = candyType.length / 2;
+  const differentCandies = new Set(candyType);
+  return Math.min(differentCandies.size, totalCandies);
+};
 
-}
-
-const candyType = [1,1,2,2,3,3]
-console.log(distributeCandies(candyType)) // Output: 3
-// Explanation: Alice can only eat 6 / 2 = 3 candies. 
+const candyType = [1, 1, 2, 2, 3, 3];
+console.log(distributeCandies(candyType)); // Output: 3
+// Explanation: Alice can only eat 6 / 2 = 3 candies.
 // Since there are only 3 types, she can eat one of each type.
