168816a719c82d1cd7a24e630c46acba4a18f4eb
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-17 12:53:42 -0400

    halves are alike setup

---
+++ determine-if-string-halves-are-alike.js
@@ -0,0 +1,8 @@
+const halvesAreAlike = (string) = {
+
+}
+
+const s = "book"
+console.log(halvesAreAlike(s))  // Output: true
+// Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. 
+// Therefore, they are alike.
--- numbers-smaller-than-current.py
+++ numbers-smaller-than-current.py
@@ -1,5 +1,7 @@
-def smallerNumbersThanCurrent(self, nums):
-    pass
+def smallerNumbersThanCurrent(nums):
+    nums = sorted(nums)
+    return nums
+        
 
 
 nums = [8,1,2,2,3]
