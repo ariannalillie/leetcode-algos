23fc57cdff4e6255b72c4abb59091d5af9492958
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-17 13:07:53 -0400

    determine if halves are equal done

--- determine-if-string-halves-are-alike.js
+++ determine-if-string-halves-are-alike.js
@@ -1,8 +1,30 @@
-const halvesAreAlike = (string) = {
+const halvesAreAlike = (string) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count1 = 0;
+  let count2 = 0;
 
-}
+  let stringArr = string.split("");
 
-const s = "book"
-console.log(halvesAreAlike(s))  // Output: true
-// Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. 
+  const half = Math.ceil(stringArr.length / 2);
+  const firstHalf = stringArr.splice(0, half);
+  const secondHalf = stringArr.splice(-half);
+
+  firstHalf.forEach((letter) => {
+    if (vowels.includes(letter.toLowerCase())) {
+      count1++;
+    }
+  });
+
+  secondHalf.forEach((letter) => {
+    if (vowels.includes(letter.toLowerCase())) {
+      count2++;
+    }
+  });
+
+  return count1 === count2;
+};
+
+const s = "AbCdEfGh";
+console.log(halvesAreAlike(s)); // Output: true
+// Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel.
 // Therefore, they are alike.
