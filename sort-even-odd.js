7a0e9bbaa771c333382413539bda7f35678e1975
Arianna Johnson <ariannajohnson588@gmail.com>
2022-12-05 21:42:41 -0500

    working on swapping odds

--- sort-even-odd.js
+++ sort-even-odd.js
@@ -1,7 +1,22 @@
 const sortEvenOdd = (nums) => {
-    return nums
+  nums.forEach((num1) => {
+    if (num1 % 2 !== 0) {
+      nums.forEach(num2 => {
+        if (num1 < num2 && num2 % 2 !== 0) {
+            swap(num1, num2)
+        }
+      })
+    }
+  });
 };
 
+const swap = (num1, num2) => {
+//   const temp = num1;
+//   num1 = num2;
+//   num2 = temp;
+//   return num1, num2;
+console.log('I am swapping', num1, 'and', num2)
+};
 
-const nums = [4,1,2,3]
-console.log(sortEvenOdd(nums))
+const nums = [4, 1, 2, 3, 7];
+console.log(sortEvenOdd(nums));
