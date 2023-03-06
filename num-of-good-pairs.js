668a1a07debc7a0f32622fd394ad152be4371d8f
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-05 19:35:59 -0500

    add: complete num of good pairs

--- num-of-good-pairs.js
+++ num-of-good-pairs.js
@@ -1,7 +1,15 @@
 const numberOfGoodPairs = (nums) => {
+  let count = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    const num1 = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      const num2 = nums[j];
+      if (num1 === num2) count++;
+    }
+  }
+  return count;
+};
 
-}
-
-const nums = [1,2,3,1,1,3]
-console.log(numberOfGoodPairs(nums)) // Output: 4
+const nums = [1, 2, 3, 1, 1, 3];
+console.log(numberOfGoodPairs(nums)); // Output: 4
 // Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
