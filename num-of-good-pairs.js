2c2dd4eea96931ccbc3e24c8730b24ab59e8686e
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-05 19:27:12 -0500

    add: num of good pairs setup

---
+++ num-of-good-pairs.js
@@ -0,0 +1,7 @@
+const numberOfGoodPairs = (nums) => {
+
+}
+
+const nums = [1,2,3,1,1,3]
+console.log(numberOfGoodPairs(nums)) // Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
