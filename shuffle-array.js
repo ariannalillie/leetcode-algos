263dcd056e8ff0a119f08a201623a0abda79b72b
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-07 13:11:19 -0500

    fix: syntax

--- shuffle-array.js
+++ shuffle-array.js
@@ -5,13 +5,13 @@ const shuffleArray = (nums, n) => {
   const secondHalf = nums.slice(n);
 
   for (let i = 0; i < firstHalf.length; i++) {
-    shuffleArray.push(firstHalf(i));
-    shuffleArray.push(secondHalf(i));
+    shuffleArray.push(firstHalf[i]);
+    shuffleArray.push(secondHalf[i]);
   }
   return shuffleArray;
 };
 
-const nums = [2,5,1,3,4,7]
-const n = 3
-console.log(shuffleArray(nums, n)) // Output: [2,3,5,4,1,7] 
+const nums = [2, 5, 1, 3, 4, 7];
+const n = 3;
+console.log(shuffleArray(nums, n)); // Output: [2,3,5,4,1,7]
 // Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
