60743d53d47a2b8df37ed10cbf9de49185eb2e76
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-08 08:56:49 -0500

    sorted peopled complete

--- sort-the-people.js
+++ sort-the-people.js
@@ -1,8 +1,23 @@
-const sortPeople = () => {
-    
-}
+const sortPeople = (names, heights) => {
+  let peopleArr = [];
+  for (let i = 0; i < names.length; i++) {
+    peopleArr.push({
+      name: names[i],
+      height: heights[i],
+    });
+  }
+
+  const sortedArr = peopleArr.sort((a, b) => b.height - a.height);
+
+  const sorted = [];
+  sortedArr.forEach((item) => {
+    sorted.push(item.name);
+  });
+
+  return sorted;
+};
 
 const names = ["Mary","John","Emma"]
 const heights = [180,165,170]
-console.log(sortPeople(name, heights)) // Output: ["Mary","Emma","John"]
+console.log(sortPeople(names, heights)); // Output: ["Mary","Emma","John"]
 // Explanation: Mary is the tallest, followed by Emma and John.
