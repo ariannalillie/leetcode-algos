c4e2681fc969ca44e45e4a57f1f18c7201d112b2
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-08 07:58:47 -0500

    sort people setup

---
+++ sort-the-people.js
@@ -0,0 +1,8 @@
+const sortPeople = () => {
+    
+}
+
+const names = ["Mary","John","Emma"]
+const heights = [180,165,170]
+console.log(sortPeople(name, heights)) // Output: ["Mary","Emma","John"]
+// Explanation: Mary is the tallest, followed by Emma and John.
