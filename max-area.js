0f595220134d7d3d035fa998a2c6491e877a4593
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-07 21:12:54 -0500

    max area setup

---
+++ max-area.js
@@ -0,0 +1,8 @@
+const maxArea = (height) => {
+
+}
+
+const height = [1,8,6,2,5,4,8,3,7]
+console.log(maxArea(height)) // Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+// In this case, the max area of water (blue section) the container can contain is 49.
