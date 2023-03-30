74fd3b48d994555bc550aaf433f49b4d9271762d
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-30 13:37:34 -0400

    count matches solution

--- count-matches.js
+++ count-matches.js
@@ -1,10 +1,24 @@
-const countMatches = () => {
+const countMatches = (items, ruleKey, ruleValue) => {
+  let ruleIndex;
+  let count = 0;
+  if (ruleKey === "type") ruleIndex = 0;
+  if (ruleKey === "color") ruleIndex = 1;
+  if (ruleKey === "name") ruleIndex = 2;
 
-}
+  items.forEach((item) => {
+    if (item[ruleIndex] === ruleValue) count++;
+  });
 
-const items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]]
-const ruleKey = "type"
-const ruleValue = "phone"
-console.log(countMatches(items, ruleKey, ruleValue)) // Output: 2
-// Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] 
+  return count;
+};
+
+const items = [
+  ["phone", "blue", "pixel"],
+  ["computer", "silver", "phone"],
+  ["phone", "gold", "iphone"],
+];
+const ruleKey = "type";
+const ruleValue = "phone";
+console.log(countMatches(items, ruleKey, ruleValue)); // Output: 2
+// Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"]
 // and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
