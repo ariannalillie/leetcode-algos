203ed1aa8eb1f95830068c3252d72983b9da82fa
Arianna Johnson <ariannajohnson588@gmail.com>
2023-03-30 12:26:41 -0400

    left right diff setup

---
+++ left-right-difference.js
@@ -0,0 +1,8 @@
+const leftRightDifference = (nums) => {
+    
+};
+
+const nums = [10, 4, 8, 3];
+console.log(leftRightDifference(nums)); // Output: [15,1,11,22]
+// Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+// The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
