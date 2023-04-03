b052a7f809a7eaf870ab3365155c259fea4cd99e
Arianna Johnson <ariannajohnson588@gmail.com>
2023-04-03 08:51:35 -0400

    working on num resue boats

--- boats-to-save-people.js
+++ boats-to-save-people.js
@@ -1,5 +1,18 @@
 const numRescueBoats = (people, limit) => {
-  
+    let boats = 0;
+    let peopleOnBoat = 0
+    let weight = 0;
+    people.forEach((person, index) => {
+        if (!(weight < limit) || peopleOnBoat === 2) {
+            peopleOnBoat === 0;
+            weight = 0;
+            boats++
+            return;
+        }
+        weight += index
+        peopleOnBoat ++
+    })
+    return boats
 };
 
 const people = [3,2,2,1];
