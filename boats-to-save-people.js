778bb9906e9f5c53436ca4d38c8cf83c98b0f426
Arianna Johnson <ariannajohnson588@gmail.com>
2023-04-03 08:42:33 -0400

    set up num rescue boats

---
+++ boats-to-save-people.js
@@ -0,0 +1,8 @@
+const numRescueBoats = (people, limit) => {
+  
+};
+
+const people = [3,2,2,1];
+const limit = 3;
+console.log(numRescueBoats(people, limit)) // Output: 3
+// Explanation: 3 boats (1, 2), (2) and (3)
