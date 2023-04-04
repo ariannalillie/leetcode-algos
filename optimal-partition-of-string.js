f85be9977070062a4c251fdecb8d4ff2e6ee3a15
Arianna Johnson <ariannajohnson588@gmail.com>
2023-04-04 08:48:58 -0400

    partition of a string setup

---
+++ optimal-partition-of-string.js
@@ -0,0 +1,9 @@
+const partitionString = (s) => {
+    
+};
+
+const s = "abacaba"
+console.log(partitionString(s)) // Output: 4
+// Explanation:
+// Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+// It can be shown that 4 is the minimum number of substrings needed.
