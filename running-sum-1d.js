ea87197192b17149aedd58cde23d916a59ae0d8f
Arianna Johnson <ariannajohnson588@gmail.com>
2023-04-04 09:52:45 -0400

    running sum working solution 2 using array.map

--- running-sum-1d.js
+++ running-sum-1d.js
@@ -1,13 +1,25 @@
 const runningSum = (nums) => {
-    let total = 0
-    let sum = []
-    nums.forEach(num => {
-        total += num;
-        sum.push(total)
-    })
-    return sum;
-}
+  let total = 0;
+  let sum = [];
+  nums.forEach((num) => {
+    total += num;
+    sum.push(total);
+  });
+  return sum;
+};
 
-const nums = [1,2,3,4];
-console.log(runningSum(nums)) // Output: [1,3,6,10]
+const nums = [1, 2, 3, 4];
+console.log(runningSum(nums)); // Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+const runningSum2 = (nums) => {
+  let total = 0;
+
+  return nums.map((num) => {
+    return (total += num);
+  });
+};
+
+const nums2 = [1, 2, 3, 4];
+console.log(runningSum2(nums2)); // Output: [1,3,6,10]
 // Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
