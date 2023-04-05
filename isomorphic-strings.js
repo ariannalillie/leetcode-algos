a1fe9dd8379e6ad428a36199c9fcffc36f5b467c
Arianna Johnson <ariannajohnson588@gmail.com>
2023-04-05 08:41:56 -0400

    isomorphic strings set up

---
+++ isomorphic-strings.js
@@ -0,0 +1,10 @@
+const isIsomorphic = (s, t) => {
+    
+};
+
+const s = "egg";
+const t = "add";
+console.log(isIsomorphic(s, t)); //Output: true
+const s2 = "foo";
+const t2 = "bar";
+console.log(isIsomorphic(s2, t2)); // Output: false
