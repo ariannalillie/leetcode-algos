fd14c8cc14185cea3dd6545e2f5ca8e2806cc367
Arianna Johnson <ariannajohnson588@gmail.com>
2023-04-05 08:26:08 -0400

    complete isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,11 +1,28 @@
 // Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-// A subsequence of a string is a new string that is formed from the original string by deleting some 
-// (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+// A subsequence of a string is a new string that is formed from the original string by deleting some
+// (can be none) of the characters without disturbing the relative positions of the remaining characters.
 // (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-const isSubsequence = (s, t) => {};
+const isSubsequence = (s, t) => {
+  let indexOfString = 0;
+  let count = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    if (indexOfString <= s.length) {
+      if (s[indexOfString] === t[i]) {
+        indexOfString++;
+        count++;
+      }
+    }
+  }
+  return count === s.length;
+};
 
 const s = "abc";
 const t = "ahbgdc";
-console.log(isSubsequence(s, t)) // Output: true;
+
+const s2 = "axc";
+const t2 = "ahbgdc";
+console.log(isSubsequence(s, t)); // Output: true;
+console.log(isSubsequence(s2, t2)); // Output: false
